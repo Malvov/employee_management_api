8c112593057651eb8689b5c9b32f41843e42996c
malvov <manuel.olivaresbonilla@gmail.com>
2019-03-16 16:31:26 -0600

    token authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+
+  # Home controller routes.
+  root   'home#index'
+  get    'auth', to: 'home#auth'
+
+  concern :api_base do      
+      # Get login token from Knock
+      post   'user_token', to: 'user_token#create'
+      
+      # User actions
+      get    '/users', to: 'users#index'
+      get    '/users/current', to: 'users#current'
+      post   '/users/create', to: 'users#create'
+      patch  '/user/:id', to: 'users#update'
+      delete '/user/:id', to: 'users#destroy'
+  end
+  
+  namespace :api do
+    namespace :v1 do
+      concerns :api_base
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
