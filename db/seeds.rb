fbd0f4cb227f10906590730a2f1d912193734517
malvov <manuel.olivaresbonilla@gmail.com>
2019-03-20 18:49:01 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Admin user
+
+User.create!(username: 'Admin user', email: 'mail1@mail.com', password: 'password', role: 'admin', employee_attributes: { first_name: 'Admin', last_name: 'User', 
+entry_date: '19970303', active: true })
